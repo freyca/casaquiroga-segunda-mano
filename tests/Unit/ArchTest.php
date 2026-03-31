dcd6cbf66b4f15664611c1a4ccc2e5ea7953690c
Fran Rey <franreycastedo@gmail.com>
2026-03-31 21:35:28 +0200

    add: arch testing

---
+++ tests/Unit/ArchTest.php
@@ -0,0 +1,24 @@
+<?php
+
+arch()
+    ->expect('App')
+    ->toUseStrictTypes()
+    ->not->toUse(['die', 'dd', 'dump']);
+
+arch()
+    ->expect('App\Models')
+    ->toBeClasses()
+    ->toExtend('Illuminate\Database\Eloquent\Model')
+    //->toOnlyBeUsedIn('App\Repositories')
+    ->ignoring('App\Models\User');
+
+arch()
+    ->expect('App\Http')
+    ->toOnlyBeUsedIn('App\Http');
+
+//arch()
+//    ->expect('App\*\Traits')
+//    ->toBeTraits();
+
+arch()->preset()->php();
+arch()->preset()->security()->ignoring('md5');
