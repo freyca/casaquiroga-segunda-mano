0032d870e3680fde5051384f592d5ad4af4616a0
Fran Rey <franreycastedo@gmail.com>
2026-04-01 17:11:06 +0200

    add: protected route

--- tests/Feature/Controllers/SecondHandMachinePrintControllerTest.php
+++ tests/Feature/Controllers/SecondHandMachinePrintControllerTest.php
@@ -3,12 +3,37 @@
 declare(strict_types=1);
 
 use App\Models\SecondHandMachine;
+use App\Models\User;
 use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
 
 uses(LazilyRefreshDatabase::class);
 
 describe('SecondHandMachinePrintController', function (): void {
+    it('route is not accessible without authentication', function (): void {
+        $machine = SecondHandMachine::factory()->create();
+
+        $response = $this->get(route('secondhandmachines.print', $machine));
+        $response->assertRedirect(route('login'));
+    });
+
+    it('route is accessible for logged in users', function (): void {
+        $machine = SecondHandMachine::factory()->create();
+
+        $this->actingAs(User::factory()->admin()->create());
+        $response = $this->get(route('secondhandmachines.print', $machine));
+        $response->assertOk();
+
+        $this->actingAs(User::factory()->employee()->create());
+        $response = $this->get(route('secondhandmachines.print', $machine));
+        $response->assertOk();
+
+        $this->actingAs(User::factory()->user()->create());
+        $response = $this->get(route('secondhandmachines.print', $machine));
+        $response->assertOk();
+    });
+
     it('displays machine and default fields', function (): void {
+        $this->actingAs(User::factory()->admin()->create());
         $machine = SecondHandMachine::factory()->create();
 
         $response = $this->get(route('secondhandmachines.print', $machine));
@@ -31,6 +56,7 @@ describe('SecondHandMachinePrintController', function (): void {
     });
 
     it('respects show_fields query param', function (): void {
+        $this->actingAs(User::factory()->admin()->create());
         $machine = SecondHandMachine::factory()->create();
         $show_fields = ['brand', 'model'];
         $dont_show_fields = ['photos', 'identifier_code', 'work_hours', 'selling_price', 'description', 'sell_status'];
