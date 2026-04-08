b9334ee5759f306976cc8b7c3a27f24c261048f3
Fran Rey <franreycastedo@gmail.com>
2026-04-08 14:05:35 +0200

    add: modified translations

--- config/app.php
+++ config/app.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'CQ Second Hand'),
+    'name' => env('APP_NAME', 'Maquinaria reacondicionada Casa Quiroga'),
 
     /*
     |--------------------------------------------------------------------------
