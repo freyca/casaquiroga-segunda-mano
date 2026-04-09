85301cf88bd5b2844eba441aba7870eedbcfecf9
Fran Rey <franreycastedo@gmail.com>
2026-04-09 08:36:40 +0200

    add: language switch

---
+++ app/Providers/FilamentLanguageSwitcherProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Providers;
+
+use BezhanSalleh\LanguageSwitch\LanguageSwitch;
+use Illuminate\Support\ServiceProvider;
+
+final class FilamentLanguageSwitcherProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        LanguageSwitch::configureUsing(function (LanguageSwitch $switch): void {
+            $switch->locales(['es', 'gl']);
+        });
+    }
+}
