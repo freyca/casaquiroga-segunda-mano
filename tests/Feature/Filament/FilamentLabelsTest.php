c4e1aee394f4cca6909749e3c85e72f1cdf18824
Fran Rey <franreycastedo@gmail.com>
2026-04-28 22:40:33 +0200

    add: delete non needed resources

--- tests/Feature/Filament/FilamentLabelsTest.php
+++ tests/Feature/Filament/FilamentLabelsTest.php
@@ -2,8 +2,6 @@
 
 declare(strict_types=1);
 
-use App\Filament\Admin\Resources\Brands\BrandResource;
-use App\Filament\Admin\Resources\Families\FamilyResource;
 use App\Filament\Admin\Resources\SecondHandMachines\SecondHandMachineResource as AdminSecondHandMachineResource;
 use App\Filament\Admin\Resources\Users\UserResource;
 use App\Filament\Employee\Resources\SecondHandMachines\SecondHandMachineResource as EmployeeSecondHandMachineResource;
@@ -24,26 +22,6 @@ describe('FilamentLabels', function (): void {
     it('admin second hand machine returns correct navigation group', function (): void {
         expect(AdminSecondHandMachineResource::getNavigationGroup())
             ->toBe(Str::ucfirst(__('machines_management')));
-    });
-
-    it('admin brand returns correct navigation label', function (): void {
-        expect(BrandResource::getNavigationLabel())
-            ->toBe(Str::ucfirst(__('brand')));
-    });
-
-    it('admin brand returns correct navigation group', function (): void {
-        expect(BrandResource::getNavigationGroup())
-            ->toBe(Str::ucfirst(__('machines_management')));
-    });
-
-    it('admin family returns correct navigation group', function (): void {
-        expect(FamilyResource::getNavigationGroup())
-            ->toBe(Str::ucfirst(__('machines_management')));
-    });
-
-    it('admin family returns correct navigation label', function (): void {
-        expect(FamilyResource::getNavigationLabel())
-            ->toBe(Str::ucfirst(__('family')));
     });
 
     it('admin user returns correct navigation group', function (): void {
