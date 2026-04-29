1cbe831721bfff79d5aa3fe7057895218eba7732
Fran Rey <franreycastedo@gmail.com>
2026-04-29 16:34:10 +0200

    add: user is searchable

--- app/Filament/Admin/Resources/Issues/Schemas/IssueForm.php
+++ app/Filament/Admin/Resources/Issues/Schemas/IssueForm.php
@@ -77,6 +77,7 @@ final class IssueForm
                             ->relationship('user', 'email')
                             ->required()
                             ->prefixIcon(Heroicon::User)
+                            ->searchable()
                             ->createOptionForm(
                                 UserForm::partialConfigure(
                                     Schema::make()
