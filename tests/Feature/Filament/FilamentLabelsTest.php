15ec6215c005d1803b726f3086319160f2cdaea0
Fran Rey <franreycastedo@gmail.com>
2026-04-29 18:45:59 +0200

    add: label testing

--- tests/Feature/Filament/FilamentLabelsTest.php
+++ tests/Feature/Filament/FilamentLabelsTest.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use App\Filament\Admin\Resources\Issues\IssueResource;
 use App\Filament\Admin\Resources\SecondHandMachines\SecondHandMachineResource as AdminSecondHandMachineResource;
 use App\Filament\Admin\Resources\Users\UserResource;
 use App\Filament\Employee\Resources\SecondHandMachines\SecondHandMachineResource as EmployeeSecondHandMachineResource;
@@ -22,6 +23,16 @@ describe('FilamentLabels', function (): void {
     it('admin second hand machine returns correct navigation group', function (): void {
         expect(AdminSecondHandMachineResource::getNavigationGroup())
             ->toBe(Str::ucfirst(__('machines_management')));
+    });
+
+    it('issue returns correct navigation group', function (): void {
+        expect(IssueResource::getNavigationGroup())
+            ->toBe(Str::ucfirst(__('issues_management')));
+    });
+
+    it('issue returns correct navigation label', function (): void {
+        expect(IssueResource::getNavigationLabel())
+            ->toBe(Str::ucfirst(__('issue')));
     });
 
     it('admin user returns correct navigation group', function (): void {
