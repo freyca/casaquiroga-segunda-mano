278c2d75910215f94ff41972bef41b295b70901a
Fran Rey <franreycastedo@gmail.com>
2026-05-04 23:05:42 +0200

    fix: format

--- app/Actions/UniqueReferenceNumber.php
+++ app/Actions/UniqueReferenceNumber.php
@@ -8,6 +8,6 @@ final class UniqueReferenceNumber
 {
     public static function create(string $type): string
     {
-        return sprintf('%s-%s', $type, now()->format('ymd-His-u'));
+        return sprintf('%s-%s-%03d-%04d', $type, now()->format('y'), now()->dayOfYear, random_int(0, 9999));
     }
 }
